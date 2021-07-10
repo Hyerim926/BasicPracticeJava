f944a7c76e9ac4f843e5ddee8a410be36fba2ca3
Hyerim926 <82012938+Hyerim926@users.noreply.github.com>
2021-07-11 03:01:16 +0900

    Add files via upload

---
+++ _0711/ifEx/IfExample1.java
@@ -0,0 +1,15 @@
+package _0711.ifEx;
+// 나이에 따라 다른 문장 출력하기
+public class IfExample1 {
+
+	public static void main(String[] args) {
+		int age = 7;
+		if(age>=8) {
+			System.out.println("학교에 다닙니다.");
+		}
+		else {
+			System.out.println("학교에 다니지 않습니다."); // 출력값
+		}
+	}
+
+}
